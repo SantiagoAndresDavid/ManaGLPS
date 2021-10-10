787ae1855309a73c9192f410a731b2e3e72dbf3c
Andres Alvarez <andresjalvarez@unicesar.edu.co>
2021-10-10 09:34:09 -0500

    clase usuario agregada ( esta clase esta sujeta a cambio)

---
+++ Entity/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Entity
+{
+    public class Usuario
+    {
+        public string NombreDeUsuario { get; set; }
+        public string Contraseña { get; set; }
+        public string TipoDeUsuario { get; set; }
+        public string Email { get; set; }
+
+        public Usuario(string nombreDeUsuario, string tipoDeUsuario, string contraseña, string correo)
+        {
+           
+            this.NombreDeUsuario = nombreDeUsuario;
+            this.TipoDeUsuario = tipoDeUsuario;
+            this.Contraseña = contraseña;
+            this.Email = correo;
+        }
+    }
+
+}
