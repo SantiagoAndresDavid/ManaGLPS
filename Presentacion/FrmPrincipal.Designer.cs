ec0fde5ced35c1a7181b3e5fe11e6b4f6969baf9
santiago <sadavid@unicesar.edu.co>
2021-10-10 11:54:42 -0500

    se agrego Interfaz Menu Principal (Esta sujeta a cambios)

---
+++ Presentacion/FrmPrincipal.Designer.cs
@@ -0,0 +1,274 @@
+﻿
+namespace Presentacion
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BtnConsulta = new System.Windows.Forms.Button();
+            this.BtnAgregar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.BtnModificar = new System.Windows.Forms.Button();
+            this.BtnConsultarExpediente = new System.Windows.Forms.Button();
+            this.BtnConsultarDatosConsulta = new System.Windows.Forms.Button();
+            this.BtnAgregarExpediente = new System.Windows.Forms.Button();
+            this.BtnAgregarDatosConsulta = new System.Windows.Forms.Button();
+            this.BtnDescativarExpediente = new System.Windows.Forms.Button();
+            this.BtnDescativarDatosConsulta = new System.Windows.Forms.Button();
+            this.BtnModificarExpediente = new System.Windows.Forms.Button();
+            this.BtnModificarDatosConsulta = new System.Windows.Forms.Button();
+            this.BtnCerrarSesion = new System.Windows.Forms.Button();
+            this.BtnAcercaDe = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel2.Controls.Add(this.BtnAcercaDe);
+            this.panel2.Controls.Add(this.BtnCerrarSesion);
+            this.panel2.Controls.Add(this.BtnModificarDatosConsulta);
+            this.panel2.Controls.Add(this.BtnModificarExpediente);
+            this.panel2.Controls.Add(this.BtnDescativarDatosConsulta);
+            this.panel2.Controls.Add(this.BtnDescativarExpediente);
+            this.panel2.Controls.Add(this.BtnAgregarDatosConsulta);
+            this.panel2.Controls.Add(this.BtnAgregarExpediente);
+            this.panel2.Controls.Add(this.BtnConsultarDatosConsulta);
+            this.panel2.Controls.Add(this.BtnConsultarExpediente);
+            this.panel2.Controls.Add(this.BtnModificar);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.BtnAgregar);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.BtnConsulta);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(199, 788);
+            this.panel2.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(109)))), ((int)(((byte)(190)))));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(199, 60);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(109)))), ((int)(((byte)(190)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(199, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1241, 60);
+            this.panel1.TabIndex = 3;
+            // 
+            // BtnConsulta
+            // 
+            this.BtnConsulta.BackColor = System.Drawing.Color.Transparent;
+            this.BtnConsulta.FlatAppearance.BorderSize = 0;
+            this.BtnConsulta.Location = new System.Drawing.Point(0, 66);
+            this.BtnConsulta.Name = "BtnConsulta";
+            this.BtnConsulta.Size = new System.Drawing.Size(199, 44);
+            this.BtnConsulta.TabIndex = 4;
+            this.BtnConsulta.Text = "Consultar";
+            this.BtnConsulta.UseVisualStyleBackColor = false;
+            this.BtnConsulta.Click += new System.EventHandler(this.BtnConsulta_Click);
+            // 
+            // BtnAgregar
+            // 
+            this.BtnAgregar.BackColor = System.Drawing.Color.Transparent;
+            this.BtnAgregar.FlatAppearance.BorderSize = 0;
+            this.BtnAgregar.Location = new System.Drawing.Point(0, 212);
+            this.BtnAgregar.Name = "BtnAgregar";
+            this.BtnAgregar.Size = new System.Drawing.Size(199, 39);
+            this.BtnAgregar.TabIndex = 5;
+            this.BtnAgregar.Text = "Agregar";
+            this.BtnAgregar.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Location = new System.Drawing.Point(-3, 364);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(202, 39);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Desactivar o Activar";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // BtnModificar
+            // 
+            this.BtnModificar.BackColor = System.Drawing.Color.Transparent;
+            this.BtnModificar.FlatAppearance.BorderSize = 0;
+            this.BtnModificar.Location = new System.Drawing.Point(0, 523);
+            this.BtnModificar.Name = "BtnModificar";
+            this.BtnModificar.Size = new System.Drawing.Size(199, 39);
+            this.BtnModificar.TabIndex = 7;
+            this.BtnModificar.Text = "Modificar";
+            this.BtnModificar.UseVisualStyleBackColor = false;
+            // 
+            // BtnConsultarExpediente
+            // 
+            this.BtnConsultarExpediente.Location = new System.Drawing.Point(23, 116);
+            this.BtnConsultarExpediente.Name = "BtnConsultarExpediente";
+            this.BtnConsultarExpediente.Size = new System.Drawing.Size(154, 42);
+            this.BtnConsultarExpediente.TabIndex = 4;
+            this.BtnConsultarExpediente.Text = "Expediente";
+            this.BtnConsultarExpediente.UseVisualStyleBackColor = true;
+            // 
+            // BtnConsultarDatosConsulta
+            // 
+            this.BtnConsultarDatosConsulta.Location = new System.Drawing.Point(23, 164);
+            this.BtnConsultarDatosConsulta.Name = "BtnConsultarDatosConsulta";
+            this.BtnConsultarDatosConsulta.Size = new System.Drawing.Size(154, 42);
+            this.BtnConsultarDatosConsulta.TabIndex = 5;
+            this.BtnConsultarDatosConsulta.Text = "Consulta";
+            this.BtnConsultarDatosConsulta.UseVisualStyleBackColor = true;
+            // 
+            // BtnAgregarExpediente
+            // 
+            this.BtnAgregarExpediente.Location = new System.Drawing.Point(23, 257);
+            this.BtnAgregarExpediente.Name = "BtnAgregarExpediente";
+            this.BtnAgregarExpediente.Size = new System.Drawing.Size(154, 42);
+            this.BtnAgregarExpediente.TabIndex = 8;
+            this.BtnAgregarExpediente.Text = "Expediente";
+            this.BtnAgregarExpediente.UseVisualStyleBackColor = true;
+            // 
+            // BtnAgregarDatosConsulta
+            // 
+            this.BtnAgregarDatosConsulta.Location = new System.Drawing.Point(23, 305);
+            this.BtnAgregarDatosConsulta.Name = "BtnAgregarDatosConsulta";
+            this.BtnAgregarDatosConsulta.Size = new System.Drawing.Size(154, 42);
+            this.BtnAgregarDatosConsulta.TabIndex = 9;
+            this.BtnAgregarDatosConsulta.Text = "Consulta";
+            this.BtnAgregarDatosConsulta.UseVisualStyleBackColor = true;
+            // 
+            // BtnDescativarExpediente
+            // 
+            this.BtnDescativarExpediente.Location = new System.Drawing.Point(23, 409);
+            this.BtnDescativarExpediente.Name = "BtnDescativarExpediente";
+            this.BtnDescativarExpediente.Size = new System.Drawing.Size(154, 42);
+            this.BtnDescativarExpediente.TabIndex = 10;
+            this.BtnDescativarExpediente.Text = "Expediente";
+            this.BtnDescativarExpediente.UseVisualStyleBackColor = true;
+            // 
+            // BtnDescativarDatosConsulta
+            // 
+            this.BtnDescativarDatosConsulta.Location = new System.Drawing.Point(23, 457);
+            this.BtnDescativarDatosConsulta.Name = "BtnDescativarDatosConsulta";
+            this.BtnDescativarDatosConsulta.Size = new System.Drawing.Size(154, 42);
+            this.BtnDescativarDatosConsulta.TabIndex = 11;
+            this.BtnDescativarDatosConsulta.Text = "Consulta";
+            this.BtnDescativarDatosConsulta.UseVisualStyleBackColor = true;
+            // 
+            // BtnModificarExpediente
+            // 
+            this.BtnModificarExpediente.Location = new System.Drawing.Point(23, 568);
+            this.BtnModificarExpediente.Name = "BtnModificarExpediente";
+            this.BtnModificarExpediente.Size = new System.Drawing.Size(154, 42);
+            this.BtnModificarExpediente.TabIndex = 12;
+            this.BtnModificarExpediente.Text = "Expediente";
+            this.BtnModificarExpediente.UseVisualStyleBackColor = true;
+            // 
+            // BtnModificarDatosConsulta
+            // 
+            this.BtnModificarDatosConsulta.Location = new System.Drawing.Point(23, 616);
+            this.BtnModificarDatosConsulta.Name = "BtnModificarDatosConsulta";
+            this.BtnModificarDatosConsulta.Size = new System.Drawing.Size(154, 42);
+            this.BtnModificarDatosConsulta.TabIndex = 13;
+            this.BtnModificarDatosConsulta.Text = "Consulta";
+            this.BtnModificarDatosConsulta.UseVisualStyleBackColor = true;
+            // 
+            // BtnCerrarSesion
+            // 
+            this.BtnCerrarSesion.BackColor = System.Drawing.Color.Transparent;
+            this.BtnCerrarSesion.FlatAppearance.BorderSize = 0;
+            this.BtnCerrarSesion.Location = new System.Drawing.Point(0, 703);
+            this.BtnCerrarSesion.Name = "BtnCerrarSesion";
+            this.BtnCerrarSesion.Size = new System.Drawing.Size(199, 39);
+            this.BtnCerrarSesion.TabIndex = 14;
+            this.BtnCerrarSesion.Text = "Cerrar Sesion";
+            this.BtnCerrarSesion.UseVisualStyleBackColor = false;
+            this.BtnCerrarSesion.Click += new System.EventHandler(this.BtnCerrarSesion_Click);
+            // 
+            // BtnAcercaDe
+            // 
+            this.BtnAcercaDe.BackColor = System.Drawing.Color.Transparent;
+            this.BtnAcercaDe.FlatAppearance.BorderSize = 0;
+            this.BtnAcercaDe.Location = new System.Drawing.Point(0, 746);
+            this.BtnAcercaDe.Name = "BtnAcercaDe";
+            this.BtnAcercaDe.Size = new System.Drawing.Size(202, 39);
+            this.BtnAcercaDe.TabIndex = 15;
+            this.BtnAcercaDe.Text = "Acerca de ";
+            this.BtnAcercaDe.UseVisualStyleBackColor = false;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1440, 788);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmPrincipal";
+            this.Text = "FrmPrincipal";
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button BtnConsulta;
+        private System.Windows.Forms.Button BtnAgregar;
+        private System.Windows.Forms.Button BtnDescativarDatosConsulta;
+        private System.Windows.Forms.Button BtnDescativarExpediente;
+        private System.Windows.Forms.Button BtnAgregarDatosConsulta;
+        private System.Windows.Forms.Button BtnAgregarExpediente;
+        private System.Windows.Forms.Button BtnConsultarDatosConsulta;
+        private System.Windows.Forms.Button BtnConsultarExpediente;
+        private System.Windows.Forms.Button BtnModificar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BtnAcercaDe;
+        private System.Windows.Forms.Button BtnCerrarSesion;
+        private System.Windows.Forms.Button BtnModificarDatosConsulta;
+        private System.Windows.Forms.Button BtnModificarExpediente;
+    }
+}
