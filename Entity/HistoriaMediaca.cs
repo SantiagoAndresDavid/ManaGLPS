392281cf456d936c32cac2e709038a6787b86a2d
Andres Alvarez <andresjalvarez@unicesar.edu.co>
2021-10-19 20:20:36 -0500

    Se agregaron las clases historia clinica y paciente a la capa Entity

---
+++ Entity/HistoriaMediaca.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    class HistoriaMediaca
+    {
+        public DateTime FechaDeCreacion { get; set; }
+        public DateTime UltimaFechaDeEdicion{ get; set; }
+        public DateTime FechaDeFinalizacion { get; set; }
+        public string Titutlo{ get; set; }
+        public int Id { get; set; }
+        public bool Estado { get; set; }
+
+
+        public HistoriaMediaca(DateTime fechaDeCreacion, DateTime ultimaFechaDeEdicion, DateTime fechaDeFinalizacion, string titulo, int id, bool estado)
+        {
+
+            this.FechaDeCreacion = fechaDeCreacion;
+            this.UltimaFechaDeEdicion = ultimaFechaDeEdicion;
+            this.FechaDeFinalizacion = fechaDeFinalizacion;
+            this.Titutlo = titulo;
+            this.Id = id;
+            this.Estado = estado;
+
+
+        }
+    }
+}
---
+++ Entity/Paciente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    class Paciente
+    {
+        public int Identificacion { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public int Telefono { get; set; }
+
+
+        public Paciente(int identificacion, string nombre, string apellido, int edad, int telefono)
+        {
+
+            this.Identificacion = identificacion;
+            this.Nombre = Nombre;
+            this.Apellido = apellido;
+            this.Edad = edad;
+            this.Telefono = telefono;
+
+        }
+    }
+}
